f9427b079f3774786c12189648a08ce613f25991
Kelvin Helmut <aek6.io@gmail.com>
2019-07-21 01:25:22 -0500

    Cambio en texto principal

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -18,7 +18,7 @@ const Home = ({ portfolios }) => (
     </Helmet>
     <Wrapper isHome>
       <Title>
-        Hello, Blog!
+        I'm Kelvin Helmut
       </Title>
     </Wrapper>
     {portfolios.length >= 4 ? (
