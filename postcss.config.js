0493514c3e9b7ed9d88e43ae5b4c620c472d619b
Kelvin Helmut <aek6.io@gmail.com>
2020-06-18 08:14:25 -0500

    cssnano

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,9 @@ const tailwindcss = require("tailwindcss");
 const cssnano = require("cssnano")({ preset: 'default', });
 
 module.exports = {
-  plugins: [ tailwindcss("./tailwind.config.js") ],
-
-  ...(process.env.NODE_ENV === "production" ? [cssnano] : []),
+  plugins: [
+    tailwindcss("./tailwind.config.js"),
+    
+    ...(process.env.NODE_ENV === "production" ? [cssnano] : []),
+  ],
 };
