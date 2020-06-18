c56bb975f6ecd9cec1b15127619d5ed26ee411ae
Kelvin Helmut <aek6.io@gmail.com>
2020-06-17 22:57:42 -0500

    Deploy.js user

--- deploy.js
+++ deploy.js
@@ -1,13 +1,13 @@
 const ghpages = require('gh-pages');
-
+console.log(ghpages)
 ghpages.publish(
     '__sapper__/export',
     {
         branch: 'master',
         repo: 'KelvinHelmut.github.io.git',
         user: {
-            name: '<KelvinHelmut>',
-            email: '<aek6.io@gmail.com>'
+            name: 'KelvinHelmut',
+            email: 'aek6.io@gmail.com'
         }
     },
     () => {
