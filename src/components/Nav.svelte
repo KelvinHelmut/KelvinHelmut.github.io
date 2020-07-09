5826d947a15ee5c34875c39bd32a138ecf2abf8c
Kelvin Helmut <aek6.io@gmail.com>
2020-07-09 13:31:08 -0500

    Nav background cuando scroll baja

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,6 +1,7 @@
 <script>
-	export let segment;
+	export let segment
 	let menuVisible = false
+	let y
 
 	function toggleMenu() {
 		menuVisible = !menuVisible
@@ -27,9 +28,15 @@
 		display: block;
 		bottom: -1px;
 	}
+
+	.bg-opacity-90 {
+		opacity: 0.95;
+	}
 </style>
 
-<nav class="flex items-center justify-between flex-wrap bg-transparent p-5 fixed w-full z-10">
+<svelte:window bind:scrollY={ y }/>
+
+<nav class="flex items-center justify-between flex-wrap p-5 fixed w-full z-10 { y > 50 || menuVisible ? 'bg-gray-100 bg-opacity-90 shadow-md': 'bg-transparent' }">
 	<div class="flex items-center flex-shrink-0 text-white mr-6">
 		<svg class="fill-current h-8 w-8 mr-2 text-yellow-500" width="54" height="54" viewBox="0 0 26 26" xmlns="http://www.w3.org/2000/svg"><path d="M12 19.5c3.75 0 7.159-3.379 6.768-4.125-.393-.75-2.268 1.875-6.768 1.875s-6-2.625-6.375-1.875S8.25 19.5 12 19.5zm4.125-12c.623 0 1.125.502 1.125 1.125v1.5c0 .623-.502 1.125-1.125 1.125A1.123 1.123 0 0115 10.125v-1.5c0-.623.502-1.125 1.125-1.125zm-8.25 0C8.498 7.5 9 8.002 9 8.625v1.5c0 .623-.502 1.125-1.125 1.125a1.123 1.123 0 01-1.125-1.125v-1.5c0-.623.502-1.125 1.125-1.125zM12 0C5.381 0 0 5.381 0 12s5.381 12 12 12 12-5.381 12-12S18.619 0 12 0zm0 1.5c5.808 0 10.5 4.692 10.5 10.5S17.808 22.5 12 22.5 1.5 17.808 1.5 12 6.192 1.5 12 1.5Z"/></svg>
 		<span class="font-semibold text-xl tracking-tight">
